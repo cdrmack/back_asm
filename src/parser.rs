b5ec4e1ce1c3ded45e104553f6dc3f7f819e20ba
cdrmack <cdrmack@protonmail.com>
2022-10-22 10:30:08 +0200

    add symbol extraction to parser

--- src/parser.rs
+++ src/parser.rs
@@ -19,6 +19,26 @@ pub fn parse(contents: String) -> Vec<String> {
     parsed_result
 }
 
+// @17 -> 17
+// @sum -> sum
+// (LOOP) -> LOOP
+fn symbol(line: String) -> String {
+    if line.len() < 1 {
+        return String::from("");
+    }
+
+    if line.as_bytes()[0] == b'@' {
+        return String::from(line.get(1..).unwrap());
+    }
+
+    if line.as_bytes()[0] == b'(' {
+        let length = line.len();
+        return String::from(line.get(1..length - 1).unwrap());
+    }
+
+    String::from("")
+}
+
 fn should_ignore(line: String) -> bool {
     // ignore empty lines
     if line.len() < 1 {
@@ -59,5 +79,23 @@ mod tests {
     fn valid_symbol() {
         let line = String::from("@8");
         assert_eq!(false, should_ignore(line));
+    }
+
+    #[test]
+    fn symbol_number() {
+        let line = String::from("@8");
+        assert_eq!("8", symbol(line));
+    }
+
+    #[test]
+    fn symbol_text() {
+        let line = String::from("@sum");
+        assert_eq!("sum", symbol(line));
+    }
+
+    #[test]
+    fn symbol_symbol() {
+        let line = String::from("(LOOP)");
+        assert_eq!("LOOP", symbol(line));
     }
 }
